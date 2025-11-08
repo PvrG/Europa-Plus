bce6b4f667d879333502a39c53ec58cda99711be
UmbiMax <109347499+UmbiMax@users.noreply.github.com>
2025-11-08 16:02:41 +0500

    Update EmergencyShuttleSystem.Console.cs

--- Content.Server/Shuttles/Systems/EmergencyShuttleSystem.Console.cs
+++ Content.Server/Shuttles/Systems/EmergencyShuttleSystem.Console.cs
@@ -402,7 +402,7 @@ public sealed partial class EmergencyShuttleSystem
                 playSound: false, colorOverride: DangerColor);
 
         if (!CheckForLaunch(component))
-            _audio.PlayGlobal("/Audio/Misc/Evacotbutie.ogg", Filter.Broadcast(), recordReplay: true);
+            _audio.PlayGlobal("/Audio/Misc/Evacotbutie.ogg", Filter.Broadcast(), recordReplay: true); // europa ny Umbi_Max edit: New custom sounds
 
         UpdateAllEmergencyConsoles();
     }
